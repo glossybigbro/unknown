87c8da4e80cb062ad0d6e1da62e1e6a17e309928
glossybigbro <glossy.bigbro@gmail.com>
2024-10-25 17:04:41 +0900

    Add Compose and Coroutine setup files(logic) for build logic

---
+++ build-logic/convention/src/main/java/io/unknown/configure/CoroutineAndroid.kt
@@ -0,0 +1,19 @@
+package io.unknown.configure
+
+import io.unknown.extension.libs
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureCoroutineAndroid() {
+    configureCoroutineKotlin()
+    dependencies {
+        "implementation"(libs.findLibrary("coroutines.android").get())
+    }
+}
+
+internal fun Project.configureCoroutineKotlin() {
+    dependencies {
+        "implementation"(libs.findLibrary("coroutines.core").get())
+        "testImplementation"(libs.findLibrary("coroutines.test").get())
+    }
+}
